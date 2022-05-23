ec00df03599efbad80f61f86ae878b913a62e40c
realerdemdoruk <erdemdoruk3@gmail.com>
2022-05-23 21:20:13 +0300

    Finish

--- script.js
+++ script.js
@@ -3,31 +3,24 @@ let temizle = document.getElementById("temizle")
 let input = document.getElementById("input")
 let yazilacaklar = document.getElementById("yazilacaklar")
 
+
 gonder.addEventListener("click", function () {
 
-    // let p = document.createElement("p")
-    let p;
-    
-    // p.append(input.value)
-    // yazilacaklar.appendChild(p)
-    // p.innerHTML = input.value
-    // input.value = ""
+    let p = document.createElement("p")
+    p.append(input.value)
+    yazilacaklar.appendChild(p)
+    p.innerHTML = input.value
+    input.value = ""
 
+    p.addEventListener("dblclick", function () {
+        yazilacaklar.removeChild(p);
+    })
 
-    let verim = localStorage.setItem("veriler",input)
-    localStorage.getItem("veriler")
+    p.addEventListener("click", function () {
+        p.style.textDecoration = "line-through";
+    })
 
-
-    // p.addEventListener("dblclick", function () {
-    //     yazilacaklar.removeChild(p);
-    // })
-
-    // p.addEventListener("click", function () {
-    //     p.style.textDecoration = "line-through";
-    // })
-
-    // temizle.addEventListener("click", function () {
-    //     yazilacaklar.removeChild(p);
-    // })
-
+    temizle.addEventListener("click", function () {
+        yazilacaklar.removeChild(p);
+    })
 })
